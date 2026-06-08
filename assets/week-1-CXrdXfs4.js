e6a2bc82727bd8db370d96a679fe8a9990af4067
netor82 <netor82@users.noreply.github.com>
2026-06-08 06:32:05 +0000

    deploy: 328b796d9e7c3125237e3a06729e30552c67fc2f

---
+++ assets/week-1-CXrdXfs4.js
@@ -0,0 +1,45 @@
+const e=[{type:"invitatorio",title:"Invitatorio",content:`Venid, adoremos al Señor,
+que hizo la tierra y el cielo.`},{type:"himno",title:"Himno",content:`Dios, Señor nuestro,
+tu gloria llena el cielo,
+y en la tierra tu nombre
+es invocado con fe.
+
+Haz que tu Iglesia cante
+los milagros del Señor,
+y en el curso de los días
+sienta tu amorosa mano.`},{type:"salmodia",title:"Salmodia",content:`Salmo 92: El Señor reina
+
+El Señor reina, se vistió de majestad,
+se ciñó de poder.
+Afianzó la tierra; no se moverá.
+
+Desde siempre está firme tu trono,
+tú eres eterno.
+
+Mucho más firmes que la voz de aguas caudalosas,
+más que el furor de las olas del mar,
+firme es el Señor en las alturas.`},{type:"lectura-breve",title:"Lectura Breve",content:`De la Primera Epístola de San Pedro (1 P 1, 3-5)
+
+Bendito sea Dios, Padre de nuestro Señor Jesucristo, que en su gran misericordia nos ha hecho renacer por la resurrección de Jesucristo a una esperanza viva, a una herencia incorruptible, inmaculada e imperecedera, reservada en el cielo para vosotros.`},{type:"responsorio",title:"Responsorio",content:`V. Levántate, gloria de Jerusalén.
+R. Y brilla con la luz de la gloria del Señor.`},{type:"benedictus",title:"Cántico de Zacarías",content:`Benedictus Dominus Deus Israel
+quia visitavit et fecit redemptionem
+plebis suae,
+et erexit cornu salutis nobis
+in domo David pueri sui,
+sicut locutus est per os
+sanctorum, qui a saeculo sunt,
+prophetarum eius,
+salute ex inimicis nostris
+et de manu omnium, qui oderunt nos.`},{type:"preces",title:"Preces",content:`Glorifiquemos a Cristo, el Señor, que fue glorificado en el madero de la cruz, y dirijamos nuestras preces al Padre:
+
+- Que reine tu justicia en la tierra.
+- Que todos los pueblos reconozcan tu gloria, Señor.`},{type:"padre-nuestro",title:"Padre Nuestro",content:`Padre nuestro, que estás en el cielo,
+santificado sea tu nombre;
+venga a nosotros tu reino;
+hágase tu voluntad en la tierra como en el cielo.
+Danos hoy nuestro pan de cada día;
+perdonanos nuestras deudas,
+como nosotros perdonamos a nuestros deudores;
+no nos dejes caer en la tentación,
+y líbranos del mal.`},{type:"oracion",title:"Oración",content:"Dios omnipotente, que nos haces comenzar este día con la luz de la fe, concédenos durante su curso obrar siempre conforme a tu voluntad, para que nuestro trabajo sea grato a tus ojos. Por nuestro Señor Jesucristo."},{type:"conclusion",title:"Conclusión",content:`V. El Señor nos bendiga y nos guarde.
+R. Amén.`}];export{e as default};
